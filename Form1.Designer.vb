0fb4286bc32f768d2a40a4e1daaeccd0c40e0322
ferdi yawan <ferdytayu@gmail.com>
2022-11-03 00:10:17 +0700

    test

---
+++ Form1.Designer.vb
@@ -0,0 +1,313 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextNAMA = New System.Windows.Forms.TextBox()
+        Me.TextNPM = New System.Windows.Forms.TextBox()
+        Me.TextNILAIHADIR = New System.Windows.Forms.TextBox()
+        Me.TextNILAITUGAS = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextNILAIUTS = New System.Windows.Forms.TextBox()
+        Me.TextNILAIUAS = New System.Windows.Forms.TextBox()
+        Me.ComboJUR = New System.Windows.Forms.ComboBox()
+        Me.ComboSEM = New System.Windows.Forms.ComboBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.TextNILAIIPS = New System.Windows.Forms.TextBox()
+        Me.TextNILAIIPK = New System.Windows.Forms.TextBox()
+        Me.BtnHITUNG = New System.Windows.Forms.Button()
+        Me.BtnKEMBALI = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Modern No. 20", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(205, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(235, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "FORM PERHITUNGAN NILAI "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(33, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "NAMA"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(33, 114)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "NPM"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(33, 151)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(65, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "JURUSAN"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(33, 194)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(74, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "SEMESTER"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(354, 73)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(115, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "NILAI KEHADIRAN"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(354, 110)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(85, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "NILAI TUGAS"
+        '
+        'TextNAMA
+        '
+        Me.TextNAMA.Location = New System.Drawing.Point(190, 66)
+        Me.TextNAMA.Name = "TextNAMA"
+        Me.TextNAMA.Size = New System.Drawing.Size(142, 20)
+        Me.TextNAMA.TabIndex = 7
+        '
+        'TextNPM
+        '
+        Me.TextNPM.Location = New System.Drawing.Point(190, 107)
+        Me.TextNPM.Name = "TextNPM"
+        Me.TextNPM.Size = New System.Drawing.Size(142, 20)
+        Me.TextNPM.TabIndex = 8
+        '
+        'TextNILAIHADIR
+        '
+        Me.TextNILAIHADIR.Location = New System.Drawing.Point(496, 66)
+        Me.TextNILAIHADIR.Name = "TextNILAIHADIR"
+        Me.TextNILAIHADIR.Size = New System.Drawing.Size(142, 20)
+        Me.TextNILAIHADIR.TabIndex = 9
+        '
+        'TextNILAITUGAS
+        '
+        Me.TextNILAITUGAS.Location = New System.Drawing.Point(496, 107)
+        Me.TextNILAITUGAS.Name = "TextNILAITUGAS"
+        Me.TextNILAITUGAS.Size = New System.Drawing.Size(142, 20)
+        Me.TextNILAITUGAS.TabIndex = 10
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(354, 151)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(68, 13)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "NILAI UTS"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(354, 190)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(68, 13)
+        Me.Label9.TabIndex = 14
+        Me.Label9.Text = "NILAI UAS"
+        '
+        'TextNILAIUTS
+        '
+        Me.TextNILAIUTS.Location = New System.Drawing.Point(496, 148)
+        Me.TextNILAIUTS.Name = "TextNILAIUTS"
+        Me.TextNILAIUTS.Size = New System.Drawing.Size(142, 20)
+        Me.TextNILAIUTS.TabIndex = 15
+        '
+        'TextNILAIUAS
+        '
+        Me.TextNILAIUAS.Location = New System.Drawing.Point(496, 194)
+        Me.TextNILAIUAS.Name = "TextNILAIUAS"
+        Me.TextNILAIUAS.Size = New System.Drawing.Size(142, 20)
+        Me.TextNILAIUAS.TabIndex = 16
+        '
+        'ComboJUR
+        '
+        Me.ComboJUR.FormattingEnabled = True
+        Me.ComboJUR.Items.AddRange(New Object() {"SI", "TI", "SIA"})
+        Me.ComboJUR.Location = New System.Drawing.Point(190, 151)
+        Me.ComboJUR.Name = "ComboJUR"
+        Me.ComboJUR.Size = New System.Drawing.Size(142, 21)
+        Me.ComboJUR.TabIndex = 17
+        '
+        'ComboSEM
+        '
+        Me.ComboSEM.FormattingEnabled = True
+        Me.ComboSEM.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8"})
+        Me.ComboSEM.Location = New System.Drawing.Point(190, 194)
+        Me.ComboSEM.Name = "ComboSEM"
+        Me.ComboSEM.Size = New System.Drawing.Size(142, 21)
+        Me.ComboSEM.TabIndex = 18
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(187, 252)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(63, 13)
+        Me.Label10.TabIndex = 19
+        Me.Label10.Text = "NILAI IPS"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(401, 252)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(63, 13)
+        Me.Label11.TabIndex = 20
+        Me.Label11.Text = "NILAI IPK"
+        '
+        'TextNILAIIPS
+        '
+        Me.TextNILAIIPS.Location = New System.Drawing.Point(170, 283)
+        Me.TextNILAIIPS.Name = "TextNILAIIPS"
+        Me.TextNILAIIPS.Size = New System.Drawing.Size(101, 20)
+        Me.TextNILAIIPS.TabIndex = 21
+        '
+        'TextNILAIIPK
+        '
+        Me.TextNILAIIPK.Location = New System.Drawing.Point(381, 283)
+        Me.TextNILAIIPK.Name = "TextNILAIIPK"
+        Me.TextNILAIIPK.Size = New System.Drawing.Size(101, 20)
+        Me.TextNILAIIPK.TabIndex = 22
+        '
+        'BtnHITUNG
+        '
+        Me.BtnHITUNG.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnHITUNG.Location = New System.Drawing.Point(86, 359)
+        Me.BtnHITUNG.Name = "BtnHITUNG"
+        Me.BtnHITUNG.Size = New System.Drawing.Size(185, 58)
+        Me.BtnHITUNG.TabIndex = 23
+        Me.BtnHITUNG.Text = "&HITUNG"
+        Me.BtnHITUNG.UseVisualStyleBackColor = True
+        '
+        'BtnKEMBALI
+        '
+        Me.BtnKEMBALI.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnKEMBALI.Location = New System.Drawing.Point(381, 359)
+        Me.BtnKEMBALI.Name = "BtnKEMBALI"
+        Me.BtnKEMBALI.Size = New System.Drawing.Size(185, 58)
+        Me.BtnKEMBALI.TabIndex = 24
+        Me.BtnKEMBALI.Text = "&KEMBALI"
+        Me.BtnKEMBALI.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(662, 450)
+        Me.Controls.Add(Me.BtnKEMBALI)
+        Me.Controls.Add(Me.BtnHITUNG)
+        Me.Controls.Add(Me.TextNILAIIPK)
+        Me.Controls.Add(Me.TextNILAIIPS)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.ComboSEM)
+        Me.Controls.Add(Me.ComboJUR)
+        Me.Controls.Add(Me.TextNILAIUAS)
+        Me.Controls.Add(Me.TextNILAIUTS)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextNILAITUGAS)
+        Me.Controls.Add(Me.TextNILAIHADIR)
+        Me.Controls.Add(Me.TextNPM)
+        Me.Controls.Add(Me.TextNAMA)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextNAMA As TextBox
+    Friend WithEvents TextNPM As TextBox
+    Friend WithEvents TextNILAIHADIR As TextBox
+    Friend WithEvents TextNILAITUGAS As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextNILAIUTS As TextBox
+    Friend WithEvents TextNILAIUAS As TextBox
+    Friend WithEvents ComboJUR As ComboBox
+    Friend WithEvents ComboSEM As ComboBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents TextNILAIIPS As TextBox
+    Friend WithEvents TextNILAIIPK As TextBox
+    Friend WithEvents BtnHITUNG As Button
+    Friend WithEvents BtnKEMBALI As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+    Private Sub BtnHITUNG_Click(sender As Object, e As EventArgs) Handles BtnHITUNG.Click
+        Dim NAMA As String
+        Dim NPM As String
+        Dim JURUSAN As String
+        Dim SEMESTER As String
+        Dim KEHADIRAN As Double
+        Dim TUGAS As Double
+        Dim UTS As Double
+        Dim UAS As Double
+        Dim IPS As Double
+        Dim IPK As Double
+
+        NAMA = TextNAMA.Text
+        NPM = TextNPM.Text
+        JURUSAN = ComboJUR.Text
+        SEMESTER = ComboSEM.Text
+        KEHADIRAN = Val(TextNILAIHADIR.Text)
+        TUGAS = Val(TextNILAITUGAS.Text)
+        UTS = Val(TextNILAIUTS.Text)
+        UAS = Val(TextNILAIUAS.Text)
+        IPS = (0.1 * KEHADIRAN + 0.2 * TUGAS + 0.3 * UTS + 0.4 * UAS)
+        TextNILAIIPS.Text = IPS
+        If SEMESTER = 1 Then
+            IPK = (IPS / 1)
+        ElseIf SEMESTER = 2 Then
+            IPK = ((IPS + IPS) / 2)
+        ElseIf SEMESTER = 3 Then
+            IPK = ((IPS + IPS + IPS) / 3)
+        ElseIf SEMESTER = 4 Then
+            IPK = ((IPS + IPS + IPS + IPS) / 4)
+        ElseIf SEMESTER = 5 Then
+            IPK = ((IPS + IPS + IPS + IPS + IPS) / 5)
+        ElseIf SEMESTER = 6 Then
+            IPK = ((IPS + IPS + IPS + IPS + IPS + IPS) / 6)
+        ElseIf SEMESTER = 7 Then
+            IPK = ((IPS + IPS + IPS + IPS + IPS + IPS + IPS) / 7)
+        ElseIf SEMESTER = 8 Then
+            IPK = ((IPS + IPS + IPS + IPS + IPS + IPS + IPS + IPS) / 8)
+        End If
+        TextNILAIIPK.Text = IPK
+    End Sub
+
+    Private Sub BtnKEMBALI_Click(sender As Object, e As EventArgs) Handles BtnKEMBALI.Click
+        TextNAMA.Text = ""
+        TextNPM.Text = ""
+        ComboJUR.Text = ""
+        ComboSEM.Text = ""
+        TextNILAIHADIR.Text = ""
+        TextNILAITUGAS.Text = ""
+        TextNILAIUTS.Text = ""
+        TextNILAIUAS.Text = ""
+        TextNILAIIPS.Text = ""
+        TextNILAIIPK.Text = ""
+    End Sub
+
+
+End Class
